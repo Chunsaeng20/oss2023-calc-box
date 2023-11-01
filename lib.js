c40ce59be940d2786ed29503d2f63be1b392b09a
Chunsaeng <gabril1022@gmail.com>
2023-11-01 17:00:03 +0900

    add prime, fact function

--- lib.js
+++ lib.js
@@ -1,19 +1,34 @@
+// calculate average
 function avg(numbers)
 {
     let s = numbers.reduce((prev, curr) => prev + curr, 0);
     return s / numbers.length;
 }
 
+// decide prime or not
 function prime(num)
 {
-    // TODO
-    return false;
+    // num must greater than 0
+    if(num <= 0) return false;
+
+    // compare 2 ~ root(num) 
+    for(let i = 2; i*i <= num; i++)
+    {
+        if(num % i == 0) return false;
+    }
+    return true;
 }
 
+// calculate factorial
 function fact(num)
 {
-    // TODO
-    return -1;
+    // num must greater than 0
+    if(num < 0) return 0;
+    // 0! = 1
+    else if(num == 0) return 1;
+
+    if(num == 1) return 1;
+    else return num * fact(num - 1);
 }
 
 module.exports = {
--- lib.test.js
+++ lib.test.js
@@ -8,3 +8,23 @@ test("avg([3, 5, 7]) should be 5", () => {
 test("avg([-5, 5]) should be 0", () => {
     expect(lib.avg([-5, 5])).toBe(0);
 });
+
+test("avg([80, 95, 70, 85, 70]) should be 80", () => {
+    expect(lib.avg([80, 95, 70, 85, 70])).toBe(80);
+});
+
+test("prime(83) should be true", () => {
+    expect(lib.prime(83)).toBe(true);
+});
+
+test("prime(82) should be false", () => {
+    expect(lib.prime(82)).toBe(false);
+});
+
+test("fact(5) should be 120", () => {
+    expect(lib.fact(5)).toBe(120);
+});
+
+test("fact(15) should be 1307674368000", () => {
+    expect(lib.fact(15)).toBe(1307674368000);
+});
